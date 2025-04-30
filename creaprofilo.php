b8379ea36cbdad7be2246b00fc0144657ea35912
ida benvenuto <benvenuto.2045045@studenti.uniroma1.it>
2025-04-30 17:15:59 +0200

    complenno

--- creaprofilo.php
+++ creaprofilo.php
@@ -2,31 +2,38 @@
 session_start();
 require 'connessione.php';
 
-if (!isset($_SESSION['id_utente'])||!isset($_SESSION['user']) ) { 
+if (!isset($_SESSION['id_utente']) || !isset($_SESSION['user'])) {
   header('Location:/register.php'); 
-  exit; 
+  exit;
 }
 
 $id_utente = $_SESSION['id_utente'];
 $email = $_SESSION['user'];
 
-$query = "SELECT nickname FROM utenti WHERE email= $1 ";
-$res = pg_query_params($dbconn, $query, [ $email]);
+// Query per recuperare il nickname e la data di nascita
+$query = "SELECT nickname, data_di_nascita FROM utenti WHERE email = $1";
+$res = pg_query_params($dbconn, $query, [$email]);
 
 if ($res) {
-  $row = pg_fetch_assoc($res);
-  if ($row) {
-      $nickname = $row['nickname'];  // Recupera il nickname dalla riga risultante
-  } else {
-      $nickname = null;  // Nessun risultato trovato
-  }
+    $row = pg_fetch_assoc($res);
+    if ($row) {
+        $nickname = $row['nickname'];  // Recupera il nickname dalla riga risultante
+        $data_di_nascita = $row['data_di_nascita'];  // Recupera la data di nascita
+    } else {
+        // Nessun risultato trovato
+        $nickname = null;
+        $data_di_nascita = null;
+    }
 } else {
-  $nickname = null;  // Query fallita
+    // La query ha fallito
+    $nickname = null;
+    $data_di_nascita = null;
 }
 
 echo json_encode([
   "status" => "success",
   "nickname" => $nickname,
-  "email" => $_SESSION['user']
+  "email" => $_SESSION['user'],
+  "data_di_nascita" => $data_di_nascita
 ]);
 ?>
